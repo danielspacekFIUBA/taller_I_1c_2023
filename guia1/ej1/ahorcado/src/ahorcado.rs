23c1eee9358151b629347a3068756db9f3842f0b
danielspacek@gmail.com <dspacek@fi.uba.ar>
2023-03-15 19:41:09 -0300

    ahorcado

---
+++ guia1/ej1/ahorcado/src/ahorcado.rs
@@ -0,0 +1,52 @@
+const CHAR_INCOGNITA: char = '_';
+
+pub struct Ahorcado{
+    pub palabra: String,
+    pub letras_intentadas: Vec<char>,
+    pub letras_adivinadas: Vec<char>,
+    pub palabra_oculta_actual: String,
+    pub resuelto: bool,
+}
+
+impl Ahorcado{
+    pub fn new(palabra: String) -> Self {
+        let palabra = palabra.to_string();
+        let letras_intentadas: Vec<char> = [].to_vec();
+        let letras_adivinadas: Vec<char> = [].to_vec();
+        let palabra_oculta_actual: String = "".to_string();
+        let resuelto = false;
+        
+        let mut ahorcado = Ahorcado { palabra, letras_intentadas, letras_adivinadas, palabra_oculta_actual, resuelto };
+        ahorcado.completar_palabra();
+        ahorcado
+    }
+
+    pub fn agregar_letra(&mut self, letra: String) -> bool{
+        let letras_actuales_acertadas = self.letras_adivinadas.len();
+        let caracteres: Vec<char> = letra.chars().collect();
+        let caracter= caracteres.first().expect("No ingresó ninguna letra válida");
+        self.letras_intentadas.push(*caracter);
+        self.completar_palabra();
+        self.letras_adivinadas.len() != letras_actuales_acertadas
+    }
+
+    fn completar_palabra(&mut self) {
+        let letras_palabra: Vec<char>= self.palabra.chars().collect();
+        let mut letras_adivinadas: Vec<char> = [].to_vec();
+        let mut resuelto = true;
+        let resultado_incognita: Vec<String> = letras_palabra.iter().map(|&x| {
+            if self.letras_intentadas.contains(&x){
+                if !letras_adivinadas.contains(&x){
+                    letras_adivinadas.push(x);
+                }
+                return String::from(x);
+            }else {
+                resuelto = false;
+                return String::from(CHAR_INCOGNITA);
+            }
+        }).collect::<Vec<_>>();
+        self.palabra_oculta_actual = resultado_incognita.join(" ");
+        self.letras_adivinadas = letras_adivinadas;
+        self.resuelto = resuelto;
+    }
+}
---
+++ guia1/ej1/ahorcado/target/debug/deps/ahorcado-239ce8a520c677c9.d
@@ -0,0 +1,7 @@
+/home/daniel/Documents/taller/guia_ejercicios/guia1/ej1/ahorcado/target/debug/deps/ahorcado-239ce8a520c677c9.rmeta: src/main.rs src/words.rs src/ahorcado.rs
+
+/home/daniel/Documents/taller/guia_ejercicios/guia1/ej1/ahorcado/target/debug/deps/ahorcado-239ce8a520c677c9.d: src/main.rs src/words.rs src/ahorcado.rs
+
+src/main.rs:
+src/words.rs:
+src/ahorcado.rs:
